b9dc61db54d3f3cc29a4fb562ab55424ef0e9bba
Dany Cervantes <dcervantes95.16@gmail.com>
2021-02-23 17:55:25 -0600

    Initial Structure of Store 1,2,3 steps

--- js/test.js
+++ js/test.js
@@ -17,3 +17,28 @@ function apareceScroll(){
     })
 }
 apareceScroll();
+
+let dany = { 
+    'namer': 'daniel', 
+    'nationality' : 'MX',
+    'age' : 17,
+    'hobby' : 'dance'
+    }
+let { namer, nationality, age} = dany
+console.log(namer, nationality, age)
+console.log(dany.namer, dany.nationality, dany.age)
+
+let team1 = ['leo', 'daana', 'diana']
+let team2 = ['javi','dany','alis']
+let teamfinal = ['elena'].concat(team1, team2)
+console.log(teamfinal)
+
+let namer = 'dany'
+let age = 17 
+let obj = { namer, age}
+console.log(obj)
+
+const names = [
+    {namer = }
+]
+}
